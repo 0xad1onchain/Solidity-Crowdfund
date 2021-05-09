cb462aa9c8f8973c927639b320955056c0720be1
Aditya <adi.gupta13@gmail.com>
2021-05-09 23:49:32 +0530

    Adding header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Menu } from 'semantic-ui-react';
+import { Container, Menu, Icon } from 'semantic-ui-react';
 import { Link } from '../routes';
 
 class Header extends Component {
@@ -9,16 +9,17 @@ class Header extends Component {
             <Menu style={{ marginTop: '10px' }}>
 
                 <Link route="/">
-                    <a className="item">CrowdFund</a>
+                    
+                    <a className="item"><Icon name="ethereum" size="large"/>CrowdFund</a>
                 </Link>
 
                 <Menu.Menu position="right">
                     <Link route="/">
                         <a className="item">Campaigns</a>
                     </Link>
-
+                        <a className="item" href="https://github.com/adigupta13/Solidity-Crowdfund"><Icon name="github" size="large"/></a>
                     <Link route="/campaigns/new">
-                        <a className="item">+</a>
+                        <a className="item"><Icon name="add"/></a>
                     </Link>
                 </Menu.Menu>
 
